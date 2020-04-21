6cf81b2fa09ef877d9da2aabe748dbeaa46716e0
Koopa <codingkoopa@gmail.com>
2020-04-21 10:07:03 -0400

    Optimize CI usage.

--- CI/Analyze/ESLint.sh
+++ CI/Analyze/ESLint.sh
@@ -3,8 +3,7 @@
 # Log debug info.
 echo "Linting code with ESLint."
 
-# Install ESLint and the naming convention plugin specifically, because the rest of the app
-# dependencies aren't needed.
-npm i eslint eslint-plugin-more-naming-conventions
+# Use npm-ci for reproducible builds, and use the cache.
+npm ci --cache .npm --prefer-offline
 # Use NPM's script interface for running ESLint.
 npm run lint
