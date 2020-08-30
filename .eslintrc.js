b1cb0992110ebe18fd21b54ad42db36839f286fe
Koopa <codingkoopa@gmail.com>
2020-08-29 21:48:12 -0400

    Fix ESLintRC file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // Unlike the rest of the project, this file uses double quotes instead of backticks, because ESLint
 // expects the former for the rules.
@@ -11,8 +11,8 @@ module.exports = {
     node: true
   },
   // Use the recommended ruleset as a base.
-  extends: "eslint:recommended",
-  plugins: ["more-naming-conventions"],
+  extends: `eslint:recommended`,
+  plugins: [`more-naming-conventions`],
   parserOptions: {
     "ecmaVersion": 8
   },
@@ -23,57 +23,57 @@ module.exports = {
     // Best Practices
 
     // C++: Require type-safe comparisons.
-    "eqeqeq": "error",
+    "eqeqeq": `error`,
     // C++: Warn about unused expressions.
-    "no-unused-expressions": "warn",
+    "no-unused-expressions": `warn`,
     // Style: Allow fallthrough in switches.
-    "no-fallthrough": "off",
+    "no-fallthrough": `off`,
 
     // Strict Mode
 
     // Safety: Always use strict mode.
     "strict": [
-      "error",
-      "global"
+      `error`,
+      `global`
     ],
 
     // Variables
 
     // C++: Warn for unused variables.
-    "no-unused-vars": "warn",
+    "no-unused-vars": `warn`,
 
     // Stylistic Issues
 
     // Style: Force array newlines IF there are any existing newlines (Everything being on one line,
     // and it being a short line is alright.).
-    "array-bracket-newline": "warn",
+    "array-bracket-newline": `warn`,
     // Style: Use Allman style braces, warn otherwise.
     "brace-style": [
-      "warn",
-      "allman"
+      `warn`,
+      `allman`
     ],
     // Style: Use 2 levels of indentation, throw error otherwise.
     "indent": [
-      "error",
+      `error`,
       2
     ],
     // Consistency: Use template literals, throw error otherwise.
     "quotes": [
-      "error",
-      "backtick"
+      `error`,
+      `backtick`
     ],
     // Style: Use no more than 100 lines.
     "max-len": [
-      "error",
+      `error`,
       100
     ],
     // Style: Use spaces only.
-    "no-tabs": "error",
+    "no-tabs": `error`,
     // C++: Use semicolons.
-    "semi": "error",
+    "semi": `error`,
 
     // Custom Rules
-    "more-naming-conventions/snake-case-variables": "warn",
-    "more-naming-conventions/upper-camel-case-functions": "warn"
+    "more-naming-conventions/snake-case-variables": `warn`,
+    "more-naming-conventions/upper-camel-case-functions": `warn`
   }
 };
