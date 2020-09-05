19ed8e008af28c5da8ca149fb163dd534280f99a
Koopa <codingkoopa@gmail.com>
2020-09-05 02:18:51 -0400

    Don't use separate CI script.

--- CI/Analyze/ESLint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Log debug info.
-echo "Linting code with ESLint."
-
-# Use npm-ci for reproducible builds, and use the cache.
-npm ci --cache .npm --prefer-offline
-# Use NPM's script interface for running ESLint.
-npm run lint
